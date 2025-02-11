0ceabe0fc7dcdaefae9c02d6211d2e36289ac9e0
MARRI DEVENDER RAO <devenrao@in.ibm.com>
2025-02-11 20:59:58 +0530

    cfam read power 12

--- p12startcbs/p12startcbs.cpp
+++ p12startcbs/p12startcbs.cpp
@@ -1,15 +1,88 @@
-//This application uses test data writes to a file and invokes
-//D-Bus method CreatePELWithFFDCFiles
-//This application is used to validate logging/sbe_ffdc_handler code
+// This application uses test data writes to a file and invokes
+// D-Bus method CreatePELWithFFDCFiles
+// This application is used to validate logging/sbe_ffdc_handler code
+#include <attributes_info.H>
+#include <libphal.H>
+#include <string.h>
+#include <unistd.h>
 
-extern "C"
-{
+#include <cstring>
+#include <iostream>
+#include <vector>
+extern "C" {
 #include "libpdbg.h"
 }
-#include<iplfacade.H>
+#include <iplfacade.H>
+
+void pdbgLogCallback(int, const char* fmt, va_list ap)
+{
+    va_list vap;
+    va_copy(vap, ap);
+    std::vector<char> logData(1 + std::vsnprintf(nullptr, 0, fmt, ap));
+    std::vsnprintf(logData.data(), logData.size(), fmt, vap);
+    va_end(vap);
+    std::string logstr(logData.begin(), logData.end());
+    std::cout << "PDBG:" << logstr << std::endl;
+}
+
+static int print_target(struct pdbg_target* target, void* /* priv */)
+{
+    const char* targetname = pdbg_target_name(target);
+    if (targetname)
+    {
+        std::cout << "DEV 1238 pdbg target name is " << targetname << std::endl;
+    }
+    char tgtPhysDevPath[64];
+    if (pdbg_target_get_attribute(target, "ATTR_PHYS_DEV_PATH", 1, 64,
+                                  tgtPhysDevPath))
+    {
+        std::cout << "DEV 1238 target devpath " << tgtPhysDevPath << std::endl;
+    }
+    const char* targetPath = pdbg_target_path(target);
+    if (targetPath)
+    {
+        std::cout << "DEV 1238 pdbg target path is " << targetPath << std::endl;
+    }
+
+    size_t len;
+    const char* compatible =
+        (const char*)pdbg_target_property(target, "compatible", &len);
+    if (compatible)
+    {
+        std::cout << "DEV 1238 pdbg compatible is " << compatible << std::endl;
+    }
+
+    const char* classname = pdbg_target_class_name(target);
+    if (classname)
+    {
+        std::cout << "DEV 1238 pdbg target class is " << classname << std::endl;
+    }
+    return 0;
+}
+
+inline struct pdbg_target* get_pib_target(struct pdbg_target* target)
+{
+    struct pdbg_target* pib = NULL;
+
+    /* We assume each processor chip contains one and only one pib */
+    pdbg_for_each_target("pib", target, pib)
+    {
+        break;
+    }
+
+    if (!pib)
+    {
+        std::cout << "No pib target associated with target %s"
+                  << pdbg_target_path(target) << std::endl;
+        return NULL;
+    }
+
+    return pib;
+}
 int main()
 {
-    constexpr auto devtree = "/tmp/DEVTREE";
+    constexpr auto devtree = "/tmp/p12_fake_2.dtb";
+    // std::cout << "enter DEV 1238 main application " << std::endl;
 
     // PDBG_DTB environment variable set to CEC device tree path
     if (setenv("PDBG_DTB", devtree, 1))
@@ -18,33 +91,58 @@ int main()
         return 0;
     }
 
-    //initialize the targeting system 
+    pdbg_set_backend(PDBG_BACKEND_SBEFIFO, NULL);
+    std::cout << "calling pdbg_targets_init " << std::endl;
+    // initialize the targeting system
     if (!pdbg_targets_init(NULL))
-    {   
+    {
         std::cerr << "pdbg_targets_init failed" << std::endl;
         return 0;
     }
-    pdbg_set_backend(PDBG_BACKEND_SBEFIFO, NULL);
 
-    struct pdbg_target *hubchip;
+    pdbg_set_loglevel(PDBG_DEBUG);
+    pdbg_set_logfunc(pdbgLogCallback);
+
+    std::cout << " DEV 1238 trying to loop through all the hub chips "
+              << std::endl;
+
+    struct pdbg_target* root = pdbg_target_root();
+    // int level =0;
+    // pdbg_target_traverse(root, print_target, &level);
+
+    std::cout << "NOW LOOKING FOR hub chip " << std::endl;
+    struct pdbg_target* hubchip;
     pdbg_for_each_target("hubchip", NULL, hubchip)
     {
-        std::cout << "hubchip found " << std::endl;	
-		pdbg_target_probe(hubchip);
-		if (pdbg_target_status(hubchip) != PDBG_TARGET_ENABLED)
-		{
-			std::cout << "hubchip chip not enabled " << std::endl;	
-			return -1;
-		}
-        int ret =  ps_sppe_config_update(hubchip);
+        std::cout << "hubchip found " << std::endl;
+        pdbg_target_probe(hubchip);
+        if (pdbg_target_status(hubchip) != PDBG_TARGET_ENABLED)
+        {
+            std::cout << "hubchip chip not enabled " << std::endl;
+            return -1;
+        }
+        pdbg_target* pib = get_pib_target(hubchip);
+        pdbg_target_probe(pib);
+        if (pdbg_target_status(pib) != PDBG_TARGET_ENABLED)
+        {
+            std::cout << "pib target for hub chip not enabled " << std::endl;
+            return -1;
+        }
+        std::cout << "invoking ps_sppe_config_update HWP" << std::endl;
+        int ret = ipl::ps_sppe_config_update(hubchip);
         if (ret == 0)
         {
-            std::cout << "succes in call to HWP ps_sppe_config_update " << std::endl;
+            std::cout << "succes in call to HWP ps_sppe_config_update "
+                      << std::endl;
         }
-		break;
+        ret = ipl::ps_cbs_start(hubchip);
+        if (ret == 0)
+        {
+            std::cout << "sucess in call to ps_cbs_start " << std::endl;
+        }
+        break;
     }
 
     sleep(100);
     return 0;
 }
-
